24e85035bb8093ca87748bdc3b706e634e8f193c
leslie-alldridge <leslie.alldridge@gmail.com>
2021-06-30 09:52:27 +1200

    server running

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const fastify = require('fastify');
+const fastify = require('fastify')({ logger: true });
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
